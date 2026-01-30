f9b1a6146b60cab92f7536ecb46c6bd225bd3062
OneHTW <45687759+harishkarthiktk@users.noreply.github.com>
2026-01-30 23:51:20 +0530

    Fix import paths and add missing dependencies
    - Convert absolute imports to relative imports in authService modules
    - Add bcrypt and jinja2 to requirements.txt

--- fast_webDrop/authService/authservice/__init__.py
+++ fast_webDrop/authService/authservice/__init__.py
@@ -6,12 +6,12 @@ A production-ready authentication system that can be easily integrated into any 
 __version__ = "1.0.0"
 __author__ = "AuthService Contributors"
 
-from authservice.core import AuthManager
-from authservice.models import User, AuthResult
-from authservice.config import AuthConfig
-from authservice.logger import setup_logger, setup_logger_from_config, AuthLogger
-from authservice.hashing import hash_password, verify_password, is_bcrypt_hash, migrate_yaml_file
-from authservice.appconfig import AppConfig
+from .core import AuthManager
+from .models import User, AuthResult
+from .config import AuthConfig
+from .logger import setup_logger, setup_logger_from_config, AuthLogger
+from .hashing import hash_password, verify_password, is_bcrypt_hash, migrate_yaml_file
+from .appconfig import AppConfig
 
 __all__ = [
     'AuthManager',
